6c05711bb405e756a9345325e9a5255bda024c6e
fabcol1 <myiscrizioni2@gmail.com>
2018-01-25 13:10:12 +0100

    server multiutente

---
+++ workspace/01_Test/src/test/Message.java
@@ -0,0 +1,33 @@
+package test;
+
+import java.io.Serializable;
+
+public class Message implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String username;
+	private String textMessage;
+	
+	public Message() { }
+	
+	public Message(String username, String textMessage) {
+		this.username = username;
+		this.textMessage = textMessage;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getTextMessage() {
+		return textMessage;
+	}
+	public void setTextMessage(String textMessage) {
+		this.textMessage = textMessage;
+	}
+	
+	
+}
